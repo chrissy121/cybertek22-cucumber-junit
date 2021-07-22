528c2936ad59a4ce8d79c41b8200a8b72ca6efc2
Chrissy121 <crys121@gmail.com>
2021-07-21 21:51:26 -0400

    commit 2

--- src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -1,5 +1,8 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.LibraryLoginPage;
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,20 +11,29 @@ import java.util.List;
 import java.util.Map;
 
 public class DataTables_StepDefinitions {
-//
+
+    LibraryLoginPage loginPage = new LibraryLoginPage();
+
     @Given("user is on the login page of library app")
         public void user_is_on_the_login_page_of_library_app() {
-        System.out.println("user is on the login page");
+        String url = ConfigurationReader.getProperty("libraryUrl");
+        Driver.getDriver().get(url);
 }
     @When("user enters username and password as below")
     public void user_enters_username_and_password_as_below(Map<String, String> loginInfo) {
-        System.out.println("loginInfo.get('username') = " + loginInfo.get("username"));
-        System.out.println("loginInfo.get(\"password\") = " + loginInfo.get("password"));
+
+//        String username = loginInfo.get("username");
+//        String password = loginInfo.get("password");
+
+       loginPage.emailInput.sendKeys(loginInfo.get("username"));
+       loginPage.passwordInput.sendKeys(loginInfo.get("password"));
+
+       loginPage.signIn.click();
 
     }
     @Then("user should see title is something")
     public void user_should_see_title_is_something() {
-
+        System.out.println("title verification");
     }
     @Then("user should see below words displayed")
     public void user_should_see_below_words_displayed(List<String> listOfFruits) {
